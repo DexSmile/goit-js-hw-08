fd97429b3d87c4af4fdac6f77dbe61a29e362759
Y_Balyura <4grn2611@gmail.com>
2023-10-08 22:34:27 +0300

    second commit terminal

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';// Цей код імпортує модуль Player з бібліотеки Vimeo для роботи з відеоплеєром Vimeo.
+import throttle from 'lodash.throttle';//Імпортується функція throttle з бібліотеки Lodash, яка дозволяє обмежувати частоту виклику функції.
 
+
+const CURRENT_TIME_KEY = 'videoplayer-current-time'; //Створюється константа CURRENT_TIME_KEY, яка буде використовуватись як ключ для 
+                                                     //збереження та вилучення поточного часу відтворення з локального сховища (localStorage).
+const iframe = document.querySelector('iframe'); //Виходить елемент <iframe> з DOM, що ймовірно містить відео Vimeo.
+const player = new Player(iframe, { //Створюється екземпляр плеєра Vimeo, використовуючи імпортований модуль Player, 
+  loop: true,                       //та передається елемент <iframe>. Налаштовуються опції програвача, такі як loop (повтор відтворення), 
+  fullscreen: true,                 //fullscreen (підтримка повноекранного режиму) та quality (якість відео).
+  quality: '1080p',
+});
+
+const getCurrentTime = function (currentTime) { //Це функція, яка приймає поточний час відтворення відео (у секундах) та зберігає його у 
+  const seconds = currentTime.seconds;          //локальному сховищі (localStorage) із ключем CURRENT_TIME_KEY.
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+//Встановлюється обробник події timeupdate на плеєрі. 
+//Ця подія спрацьовує при зміні поточного відтворення відео. Функція throttle використовується обмеження частоти виклику функції
+//getCurrentTime до одного разу на секунду.
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
+//Виходить значення поточного часу відтворення локального сховища (якщо воно було збережено раніше).
+//Потім встановлюється час відтворення на плеєрі за допомогою player.setCurrentTime.
+player
+  .setColor('#d8e0ff')
+  .then(function (color) {
+    console.log('The new color value: #D8E0FF');
+  })
+  .catch(function (error) {
+    console.log('An error occurred while setting the color');
+  });
+  //Встановлюється колір програвача за допомогою методу setColor. Якщо операція буде успішною, виводиться повідомлення в консоль
+  //про нове значення кольору. У разі помилки також відображається повідомлення про помилку.
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+import throttle from "lodash.throttle";
+//Тут імпортується функція throttle із бібліотеки Lodash. throttle використовується для обмеження частоти виклику функції onInputData,
+//що корисно, щоб уникнути надмірного оновлення даних у локальному сховищі при швидкому введенні.
+const LOCAL_KEY = 'feedback-form-state';
+//Ця константа використовується для завдання ключа, під яким дані форми зберігатимуться у локальному сховищі браузера.
 
+form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onFormSubmit);
+//Перший слухач відстежує подію input на формі і викликає функцію onInputData з використанням throttle. 
+//throttle дозволяє викликати наInputData не частіше, ніж раз на 500 мілісекунд, щоб зменшити навантаження на локальне сховище при частому введенні даних.
+// Другий слухач відслідковує подію підряду на формі і викликає функцію наFormSubmit, коли форма відправляється.
+
+let dataForm = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+//Цей рядок намагається отримати дані з локального сховища з ключем LOCAL_KEY і розбирає їх з JSON-рядка. 
+//Якщо дані відсутні або можуть бути розібрані, створюється порожній об'єкт {} і присвоюється змінної dataForm.
+const { email, message } = form.elements;
+//Тут використовується деструктуризація об'єкта form.elements для отримання посилань на елементи форми з іменами email та message.
+//Це дозволяє легко звертатися до даних, введених у ці поля.
+reloadPage();
+//Цей рядок викликає функцію reloadPage(), яка заповнює поля форми збереженими даними з об'єкта dataForm, якщо такі дані є.
+
+function onInputData(e) {
+    dataForm = { email: email.value, message: message.value };
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(dataForm));
+  }
+  //Ця функція викликається під час введення даних у поля форми. Вона оновлює об'єкт dataForm новими даними та зберігає їх
+  //у локальне сховище браузера, перевівши об'єкт у JSON-рядок.
+
+  function reloadPage() {
+    if (dataForm) {
+      email.value = dataForm.email || '';
+      message.value = dataForm.message || '';
+    }
+    //Ця функція заповнює поля форм даних з об'єкта dataForm, якщо такі дані є. Якщо якесь із полів відсутнє у dataForm,
+    //воно буде заповнене порожнім рядком.
+  }
+
+  function onFormSubmit(e) {
+    e.preventDefault();
+    console.log({ email: email.value, message: message.value });
+  
+    if (email.value === '' || message.value === '') {
+      return alert('Please fill in all the fields!');
+    }
+  
+    localStorage.removeItem(LOCAL_KEY);
+    e.currentTarget.reset();
+    dataForm = {};
+  }
+  //Ця функція викликається під час надсилання форми. Вона запобігає стандартній поведінці відправки форми (перезавантаження сторінки),
+  //виводить дані з полів email та message в консоль, перевіряє, що обидва поля заповнені, і якщо ні, виводить попередження.
+  //Потім видаляє дані з локального сховища за допомогою localStorage.removeItem, скидає значення полів форми і очищає об'єкт dataForm.
